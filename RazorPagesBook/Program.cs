28dd260b8eda00376d3104d19050a367348f8321
Yufang <umz54286@gmail.com>
2024-07-17 15:04:38 +0800

    Initial Commit added comments

--- RazorPagesBook/Program.cs
+++ RazorPagesBook/Program.cs
@@ -1,3 +1,4 @@
+// Create WebApplicationBuilder
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -13,13 +14,21 @@ if (!app.Environment.IsDevelopment())
     app.UseHsts();
 }
 
+// Middlewares
+// 將 HTTP 要求重新導向至 HTTPS。
 app.UseHttpsRedirection();
+
+// 允許提供靜態檔案，例如 HTML、CSS、影像和 JavaScript。
 app.UseStaticFiles();
 
+// 將 Route 比對新增至 middleware pipeline。
 app.UseRouting();
 
-app.UseAuthorization();
-
+// 設定 Razor Pages 的  endpoint routing。
 app.MapRazorPages();
 
+// 授權給使用者存取安全資源。 此應用程式不使用授權。
+//app.UseAuthorization();
+
+// 執行應用程式。
 app.Run();
